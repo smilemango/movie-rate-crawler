4327acfd6bd59f336c2ea5b0009096a7b18cbfcd
alexk <smileman.god@gmail.com>
2017-05-07 21:39:09 +0900

    word2vec으로 돌려봄.
    word2vec 쪽 소스 손봐야햠.

---
+++ word2vec_tutorial.py
@@ -0,0 +1,9 @@
+# import modules & set up logging
+import gensim, logging
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+sentences = [['first', 'sentence'], ['second', 'sentence']]
+# train word2vec on the two sentences
+model = gensim.models.Word2Vec()
+model.accuracy('questions-words.txt')
